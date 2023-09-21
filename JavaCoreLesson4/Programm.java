f2a2ba307def936965ee5308ccc1b9a392a9419f
Evgenii <eaturkin@yandex.ru>
2023-09-21 19:49:04 +0500

    Programm got some changes

--- JavaCoreLesson4/Programm.java
+++ JavaCoreLesson4/Programm.java
@@ -1,10 +1,22 @@
 package JavaCoreLesson4;
 
+import JavaCoreLesson4.Exceptions.MyArrayDataException;
+import JavaCoreLesson4.Exceptions.MyArraySizeException;
+
 public class Programm {
     public static void main(String[] args) {
-        Array newAr = new Array();
-        newAr.manualFillingArray();
-        int res = newAr.sumOfArray();
-        System.out.println(res);
+        while (true) {
+            try {
+                Array newAr = new Array();
+                newAr.manualFillingArray();
+                int res = newAr.sumOfArray();
+                System.out.println(res);
+                break;
+            } catch (MyArraySizeException e) {
+                System.out.println(e.getMessage());
+            } catch (MyArrayDataException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
